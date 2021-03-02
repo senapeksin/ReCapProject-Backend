ae3d2ff7c7f29ae91900def71b658cb275f93cbc
SENA <senapksnn@gmail.com>
2021-03-03 00:42:20 +0300

    11. ders

---
+++ WebAPI/Controllers/.vshistory/CarsController.cs/2021-03-03_00_08_30_653.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public ActionResult GetAll()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpGet("getcarsbycolorid")]
+      public ActionResult Get(int ColorId)
+        {
+            var result = _carService.GetCarsByColorId(ColorId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+
+    }
+}
